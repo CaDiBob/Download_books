5f2c01e5513eae574a6958e3a2c332efd1c08eed
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-02-13 22:56:36 +0900

    Update parse_tululu_category.py
    add skip_txt, skip_imgs, json_path and dest_folder

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -5,6 +5,7 @@ import requests
 import textwrap
 
 from bs4 import BeautifulSoup
+from pathlib import Path
 from pathvalidate import sanitize_filepath
 from urllib.parse import urljoin
 from urllib.parse import urlparse
@@ -12,8 +13,8 @@ from urllib.parse import urlparse
 
 def add_argument_parser():
     parser = argparse.ArgumentParser(
-        description='''Скрипт скачивает книги с сайта
-        https://tululu.org в указанном диапозе страниц'''
+        description='''Скрипт скачивает книги жанра "научная фантастика" 
+        с сайта https://tululu.org в указанном диапозе страниц'''
     )
     parser.add_argument(
         '--start_page',
@@ -25,7 +26,27 @@ def add_argument_parser():
         '--end_page',
         default=701+1,
         type=int,
-        help='Конечный номер страницы'
+        help='Конечный номер страницы, по умолчанию: 701',
+    )
+    parser.add_argument(
+        '--dest_folder',
+        help='Путь к каталогу "куда скачивать"',
+    )
+    parser.add_argument(
+        '--skip_imgs',
+        action='store_true',
+        default=False,
+        help='Не скачивать картинки',
+    )
+    parser.add_argument(
+        '--skip_txt',
+        action='store_true',
+        default=False,
+        help='Не скачивать книги',
+    )
+    parser.add_argument(
+        '--json_path',
+        help='Путь к .json файлу с результатами',
     )
     return parser
 
@@ -35,23 +56,23 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_txt(book_url, txt_filename, folder='books/'):
-    os.makedirs(folder, exist_ok=True)
+def download_txt(book_url, txt_filename, txt_filepath):
+    os.makedirs(txt_filepath, exist_ok=True)
     response = requests.get(book_url)
     response.raise_for_status()
     check_for_redirect(response)
     txt_filename = sanitize_filepath(txt_filename)
-    filepath = os.path.join(folder, txt_filename)
+    filepath = os.path.join(txt_filepath, txt_filename)
     with open(filepath, 'w')as file:
         file.write(response.text)
 
 
-def download_image(img_url, img_filename, folder='images/'):
-    os.makedirs(folder, exist_ok=True)
+def download_image(img_url, img_filename, img_filepath):
+    os.makedirs(img_filepath, exist_ok=True)
     response = requests.get(img_url)
     response.raise_for_status()
     check_for_redirect(response)
-    filepath = os.path.join(folder, img_filename)
+    filepath = os.path.join(img_filepath, img_filename)
     with open(filepath, 'wb')as file:
         file.write(response.content)
 
@@ -85,25 +106,25 @@ def parse_book_page(soup):
     }
 
 
-def is_json(items):
-    if os.path.exists('book_items.json'):
-        return add_to_json(items, filename='book_items.json')
+def is_json(items, json_filepath):
+    if os.path.exists(f'{json_filepath}/book_items.json'):
+        return add_to_json(items, json_filepath, filename='book_items.json')
     else:
-        return save_to_json(items, filename='book_items.json')
+        return save_to_json(items, json_filepath, filename='book_items.json')
 
 
-def add_to_json(items, filename):
-    with open(filename, 'r+') as file:
+def add_to_json(items, json_filepath, filename):
+    with open(f'{json_filepath}/{filename}', 'r+') as file:
         content = json.load(file)
         content.append(items)
         file.seek(0)
         json.dump(content, file, indent=4, ensure_ascii=False)
 
 
-def save_to_json(items, filename):
+def save_to_json(items, json_filepath, filename):
     book_items = [items]
-    with open(filename, 'w') as fp:
-        json.dump(book_items, fp, indent=4, ensure_ascii=False)
+    with open(f'{json_filepath}/{filename}', 'w') as file:
+        json.dump(book_items, file, indent=4, ensure_ascii=False)
 
 
 def get_page(start, end):
@@ -132,9 +153,23 @@ def get_urk_book(page_tags):
 def main():
     parser = add_argument_parser()
     args = parser.parse_args()
+    skip_imgs = args.skip_imgs
+    skip_txt = args.skip_txt
+    json_path = args.json_path
+    folder = args.dest_folder
     start = args.start_page
     end = args.end_page
     urls = get_page(start, end)
+    if folder:
+        txt_filepath = f'{folder}/books/'
+        img_filepath = f'{folder}/images/'
+        json_filepath = folder
+    else:
+        txt_filepath = 'books/'
+        img_filepath = 'images/'
+        json_filepath = os.getcwd()
+    if json_path:
+        json_filepath = json_path
     for url in urls:
         try:
             soup = get_book_page(url)
@@ -145,13 +180,16 @@ def main():
             img_filename = os.path.basename(raw_img_path.path)
             txt_filename = f'{title}.txt'
             txt_url = book_items.get('txt_url')
-            download_txt(txt_url, txt_filename, folder='books/')
-            download_image(img_url, img_filename, folder='images/')
+            if not skip_txt:
+                download_txt(txt_url, txt_filename, txt_filepath)
+            if not skip_imgs:
+                download_image(img_url, img_filename, img_filepath)
             book_items.update(
-                book_path=f'books/{txt_filename}',
-                img_src=f'images/{img_filename}',
+                book_path=f'{txt_filepath}/{txt_filename}',
+                img_src=f'{img_filepath}/{img_filename}',
             )
-            is_json(book_items)
+            is_json(book_items, json_filepath)
+            exit()
         except requests.HTTPError:
             continue
         except TypeError:
