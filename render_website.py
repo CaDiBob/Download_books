6411576d666b85f1d181d01be273c8cea18ccb6e
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-02-17 22:13:39 +0900

    /home/user/Python/Верстка для питониста/5 урок/books/render_website.py

--- render_website.py
+++ render_website.py
@@ -1,36 +1,35 @@
 import json
-
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
+import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server, shell
 
+
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        items=read_json('book_items.json'),
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
 
 def read_json(book_items):
     with open(book_items, 'r') as file:
         items = file.read()
-    loaded_file = json.loads(items)
-    return loaded_file
+    loaded_items = json.loads(items)
+    return loaded_items
 
 
-def main():
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-    
-    book_items = 'book_items.json'
-    items = read_json(book_items)
-    template = env.get_template('template.html')
-    rendered_page = template.render(
-        items=items,
-    )
-    
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-    
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+def main(): 
+ 
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
 
 
 if __name__ == '__main__':
