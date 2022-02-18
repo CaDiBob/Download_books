f858b0bd29dcc12dd086005303a39f66c4015574
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-02-18 16:04:02 +0900

    split books list by pages

--- render_website.py
+++ render_website.py
@@ -2,26 +2,31 @@ import json
 import os
 
 
-from more_itertools import distribute
+from more_itertools import distribute, chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
 
 
 def on_reload():
+    path = 'pages'
+    os.makedirs(path, exist_ok=True)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-    items_1, items_2 = distribute(2, read_json('book_items.json'))
-    items_1 = list(items_1)
-    items_2 = list(items_2)
-    rendered_page = template.render(
-        items_1=items_1,
-        items_2=items_2,
-    )
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    book_items = read_json('book_items.json')
+    items_1, items_2 = distribute(2, book_items)
+    items_1 = list(chunked(items_1, 20))
+    items_2 = list(chunked(items_2, 20))
+    for number, items in enumerate(zip(items_1, items_2), 1):
+        itms_1, itms_2 = items
+        rendered_page = template.render(
+            itms_1=itms_1,
+            itms_2=itms_2,
+        )
+        with open(os.path.join(path, f'index{number}.html'), 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def read_json(book_items):
