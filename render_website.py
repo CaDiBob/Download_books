46c58b0a29985631f35ff56cddab5b32b7d6829c
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-02-19 19:21:11 +0900

    pep8 correction

--- render_website.py
+++ render_website.py
@@ -15,28 +15,35 @@ def on_reload():
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-    book_items = read_json('book_items.json')
-    items_1, items_2 = distribute(2, book_items)
-    items_1 = list(chunked(items_1, 20))
-    items_2 = list(chunked(items_2, 20))
+    book_items = get_book_items('book_items.json')
+    column_items_1, column_items_2 = distribute(2, book_items)
+    column_items_1 = list(chunked(column_items_1, 20))
+    column_items_2 = list(chunked(column_items_2, 20))
 
-    for number, items in enumerate(zip(items_1, items_2), 1):
-        itms_1, itms_2 = items
+    for number, items in enumerate(zip(column_items_1, column_items_2), 1):
+        items_1, items_2 = items
         rendered_page = template.render(
-            number_pages=len(items_1),
+            number_pages=len(column_items_1),
             current_page=number,
-            itms_1=itms_1,
-            itms_2=itms_2,
+            items_1=items_1,
+            items_2=items_2,
         )
-        with open(os.path.join(path, f'index{number}.html'), 'w', encoding="utf8") as file:
+        with open(
+            os.path.join(
+                path,
+                f'index{number}.html'
+                ),
+                'w',
+                encoding="utf8"
+                ) as file:
             file.write(rendered_page)
 
 
-def read_json(book_items):
+def get_book_items(book_items):
     with open(book_items, 'r') as file:
         items = file.read()
-    loaded_items = json.loads(items)
-    return loaded_items
+    book_items = json.loads(items)
+    return book_items
 
 
 def main():
